daaa5d72af48b71be4697dc83c381d8c227772fd
Max J. Polster <max.polster@id-p.de>
2024-01-03 01:22:06 +0100

    Add create element benchmarks

--- examples/src/benchmarks/rendering.tsx
+++ examples/src/benchmarks/rendering.tsx
@@ -1,4 +1,4 @@
-import { isWritable } from "@mxjp/gluon";
+import { e, isWritable } from "@mxjp/gluon";
 import { Bench, Group, offscreenSync } from "./common";
 
 export const renderingBenches = new Group("Rendering", [
@@ -19,4 +19,71 @@ export const renderingBenches = new Group("Rendering", [
 			},
 		});
 	}),
+
+	new Group("Create Element", [
+		new Bench("document.createElement", () => {
+			return offscreenSync({
+				sampleSize: 10_000,
+				cooldown: 500,
+				cycle() {
+					const elem = document.createElement("input");
+					elem.type = "text";
+					elem.classList.add("foo", "bar", "baz");
+					elem.style.color = "red";
+					elem.style.width = "42px";
+					elem.placeholder = "Hello World!";
+					elem.value = "Some text...";
+				},
+			});
+		}),
+
+		new Bench("innerHTML", () => {
+			const host: HTMLElement = <div />;
+			return offscreenSync({
+				sampleSize: 10_000,
+				cooldown: 500,
+				cycle() {
+					host.innerHTML = `<input type="text" class="foo bar baz" style="color: red; width: 42px;" placeholder="Hello World!" value="Some text...">`;
+				},
+			});
+		}),
+
+		new Bench("gluon jsx", () => {
+			return offscreenSync({
+				sampleSize: 10_000,
+				cooldown: 500,
+				cycle() {
+					(<input
+						type="text"
+						class="foo bar baz"
+						style={{
+							color: "red",
+							width: "42px",
+						}}
+						placeholder="Hello World!"
+						value="Some text..."
+					/>);
+				},
+			});
+		}),
+
+		new Bench("gluon e", () => {
+			return offscreenSync({
+				sampleSize: 10_000,
+				cooldown: 500,
+				cycle() {
+					e("input", {
+						type: "text",
+						class: "foo bar baz",
+						style: {
+							color: "red",
+							width: "42px",
+						},
+						placeholder: "Hello World!",
+						value: "Some text...",
+					});
+				},
+			});
+		}),
+	]),
 ]);

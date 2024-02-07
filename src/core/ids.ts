eaedd06bf8c41caccaa7d4d259df59d9f3577afd
Max J. Polster <max.polster@id-p.de>
2024-02-07 23:06:20 +0100

    Improve unique id performance

--- src/core/ids.ts
+++ src/core/ids.ts
@@ -1,19 +1,24 @@
 
-const NEXT = Symbol.for("gluon:next_id");
+interface Shared {
+	next: number;
+}
+
+const KEY = Symbol.for("gluon:next_id_container");
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+const SHARED: Shared = (globalThis as any)[KEY] ?? ((globalThis as any)[KEY] = { next: 0 });
+const PREFIX = "gluon_";
 
 /**
- * Allocate an ID that is unique in the current execution context.
+ * Allocate an ID that is unique in the current thread.
  *
  * @returns The unique id.
  */
 export function uniqueId(): string {
-	const id = ((globalThis as any)[NEXT] as number | undefined) ?? 0;
-	(globalThis as any)[NEXT] = id + 1;
-	return `gluon_${id}`;
+	return PREFIX + String(SHARED.next++);
 }
 
 /**
- * Run a function with an ID that is unique in the current execution context.
+ * Run a function with an ID that is unique in the current thread.
  *
  * @param fn The function to run.
  * @returns The function's return value.

65f5211a807cc6f26d1834ebcc1d23e608610968
Max J. Polster <max.polster@id-p.de>
2024-05-10 21:13:45 +0200

    Fix gh pages fallback

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -1,5 +1,7 @@
-import { defineConfig } from "vite";
 import { doc } from "@mxjp/doc";
+import { copyFile } from "node:fs/promises";
+import { join, resolve } from "node:path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
 	root: "./src",
@@ -7,7 +9,6 @@ export default defineConfig({
 	build: {
 		outDir: "../dist",
 		target: "esnext",
-		emptyOutDir: true,
 	},
 	optimizeDeps: {
 		exclude: [
@@ -18,8 +19,24 @@ export default defineConfig({
 		port: 8080,
 	},
 	plugins: [
+		historyFallback(),
 		doc({
 			highlightLanguages: ["tsx", "bash", "html"],
 		}),
 	],
 });
+
+/** @returns {import("vite").Plugin} */
+function historyFallback() {
+	let dist;
+	return {
+		name: "github pages history fallback",
+		apply: "build",
+		configResolved(config) {
+			dist = resolve(config.root, config.build.outDir);
+		},
+		async buildEnd() {
+			await copyFile(join(dist, "index.html"), join(dist, "404.html"));
+		},
+	};
+}

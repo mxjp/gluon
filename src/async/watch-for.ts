ba47a2082027e7839cfb24cdd975eb814b362435
Max J. Polster <max.polster@id-p.de>
2024-05-16 21:36:49 +0200

    Fix wrong type names

--- src/async/watch-for.ts
+++ src/async/watch-for.ts
@@ -2,10 +2,10 @@ import { captureSelf, teardown } from "../core/lifecycle.js";
 import { Expression, watch } from "../core/signals.js";
 import { Falsy } from "../core/types.js";
 
-export type WaitGuardCondition<T, R extends T> = (value: T) => value is R;
-export type WaitCondition<T> = (value: T) => boolean;
+export type WatchGuardCondition<T, R extends T> = (value: T) => value is R;
+export type WatchCondition<T> = (value: T) => boolean;
 
-export class WaitForTimeoutError extends Error {}
+export class WatchForTimeoutError extends Error {}
 
 /**
  * Utility to watch an expression until it's output satisfies a condition.
@@ -13,22 +13,22 @@ export class WaitForTimeoutError extends Error {}
  * @param expr The expression to watch.
  * @param condition The condition to test. By default, all truthy values are matched.
  * @param timeout An optional timeout. Default is no timeout.
- * @returns A promise that resolves with the first matched output or rejects with a {@link WaitForTimeoutError}.
+ * @returns A promise that resolves with the first matched output or rejects with a {@link WatchForTimeoutError}.
  */
 export function watchFor<T>(expr: Expression<T | Falsy>, timeout?: number): Promise<T>;
-export function watchFor<T, R extends T>(expr: Expression<T>, condition?: WaitGuardCondition<T, R>, timeout?: number): Promise<R>;
-export function watchFor<T>(expr: Expression<T>, condition?: WaitCondition<T>, timeout?: number): Promise<T>;
-export function watchFor<T>(expr: Expression<T>, condition?: WaitCondition<T> | number, timeout?: number): Promise<T> {
+export function watchFor<T, R extends T>(expr: Expression<T>, condition?: WatchGuardCondition<T, R>, timeout?: number): Promise<R>;
+export function watchFor<T>(expr: Expression<T>, condition?: WatchCondition<T>, timeout?: number): Promise<T>;
+export function watchFor<T>(expr: Expression<T>, condition?: WatchCondition<T> | number, timeout?: number): Promise<T> {
 	if (typeof condition === "number") {
 		timeout = condition;
-		condition = Boolean as unknown as WaitCondition<T>;
+		condition = Boolean as unknown as WatchCondition<T>;
 	} else if (condition === undefined) {
-		condition = Boolean as unknown as WaitCondition<T>;
+		condition = Boolean as unknown as WatchCondition<T>;
 	}
 	return new Promise<T>((resolve, reject) => {
 		captureSelf(dispose => {
 			watch(expr, value => {
-				if ((condition as WaitCondition<T>)(value)) {
+				if ((condition as WatchCondition<T>)(value)) {
 					dispose();
 					resolve(value);
 				}
@@ -36,7 +36,7 @@ export function watchFor<T>(expr: Expression<T>, condition?: WaitCondition<T> | 
 			if (timeout !== undefined) {
 				const handle = setTimeout(() => {
 					dispose();
-					reject(new WaitForTimeoutError());
+					reject(new WatchForTimeoutError());
 				}, timeout);
 				teardown(() => clearTimeout(handle));
 			}

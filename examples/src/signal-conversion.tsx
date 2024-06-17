216870de2d99f92605fc9b43c250968c18c95855
Max J. Polster <max.polster@id-p.de>
2024-06-18 00:16:11 +0200

    Migrate signal conversion example

---
+++ examples/src/signal-conversion.tsx
@@ -0,0 +1,53 @@
+/*
+
+# Signal Conversion
+When building user inputs, signals are the best way to pass data in and out of your components.
+
+This example shows a "trim" function that derives a signal to automatically trim user input.
+
+*/
+
+import { Signal, sig, watchUpdates } from "@mxjp/gluon";
+
+function trim(source: Signal<string>) {
+	const input = sig(source.value);
+
+	// Trim and write into the source signal:
+	watchUpdates(input, value => {
+		source.value = value.trim();
+	});
+
+	// Write into the input when the trimmed version is different:
+	watchUpdates(source, value => {
+		if (input.value.trim() !== value) {
+			input.value = value;
+		}
+	});
+
+	return input;
+}
+
+export function Example() {
+	const text = sig("Hello World!");
+	return <div class="column">
+		<div class="row">
+			<TextInput value={text.pipe(trim)} />
+			<button $click={() => { text.value = "Hello World!" }}>Reset</button>
+		</div>
+		<div>
+			You typed: <b>{() => JSON.stringify(text.value)}</b>
+		</div>
+	</div>;
+}
+
+function TextInput(props: {
+	value: Signal<string>;
+}) {
+	return <input
+		type="text"
+		prop:value={props.value}
+		$input={event => {
+			props.value.value = (event.target as HTMLInputElement).value;
+		}}
+	/>;
+}
